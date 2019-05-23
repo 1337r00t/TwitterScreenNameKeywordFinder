27ebe6cda8fc499c4491dfe8ca0642a450d55d3d
1337r00t <u9cc85@gmail.com>
2019-05-23 09:27:23 +0300

    Developer can use ur Tool by user and pass only .

--- secrets.py
+++ secrets.py
@@ -1,4 +1,11 @@
-consumer_key = 'REPLACE_ME'
-consumer_secret = 'REPLACE_ME'
-access_token = 'REPLACE_ME'
-access_secret = 'REPLACE_ME'
+import requests
+USERNAME = 'REPLACE_ME'
+PASSWORD = 'REPLACE_ME'
+login = lambda u,p: requests.post("https://api.twitter.com/auth/1/xauth_password.json",data={'x_auth_identifier':u,'x_auth_password':p},headers={'Authorization':'Bearer AAAAAAAAAAAAAAAAAAAAAFXzAwAAAAAAMHCxpeSDG1gLNLghVe8d74hl6k4%3DRUMF4xAQLsbeBhTSRrCiQpJtxoGWeyHrDb5te2jpGskWDFW82F','X-Guest-Token':requests.post("https://api.twitter.com/1.1/guest/activate.json",headers={"Authorization":"Bearer AAAAAAAAAAAAAAAAAAAAAFXzAwAAAAAAMHCxpeSDG1gLNLghVe8d74hl6k4%3DRUMF4xAQLsbeBhTSRrCiQpJtxoGWeyHrDb5te2jpGskWDFW82F"}).json()['guest_token']}).json()
+auth = login(USERNAME,PASSWORD)
+###########################################################
+# https://gist.github.com/shobotch/5160017
+consumer_key = '3nVuSoBZnx6U4vzUxf5w'
+consumer_secret = 'Bcs59EFbbsdF6Sl9Ng71smgStWEGwXXKSjYvPVt7qys'
+access_token = auth['oauth_token']
+access_secret = auth['oauth_token_secret']
